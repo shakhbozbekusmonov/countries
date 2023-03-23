3cdfde6799bc7c98ccd054961ad2a1f08bbef94f
Shakhbozbek Usmonov <83509440+shakhbozbekusmonov@users.noreply.github.com>
2023-03-23 16:10:58 +0500

    Revert "Header done"

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const elBody = document.querySelector("body");
-const elThemeBtn = document.querySelector(".site-header__btn");
-
-elThemeBtn.addEventListener("click", () => {
-    elBody.classList.toggle("theme-dark");
-    elThemeBtn.classList.toggle("site-header__dark-btn");
-});
